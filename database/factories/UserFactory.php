7c1e5758c051aa3fad903c9c94f06a4d455952b2
parsivale313 <aminmohammadgheibi@gmail.com>
2025-07-15 14:02:35 +0330

    '10'

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstname(),
+            'last_name' => fake()->lastname(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'admin' => false,
         ];
     }
 
@@ -41,4 +43,12 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    public function admin(): static 
+    {
+
+        return $this->state(fn (array $attributes) => [
+            'admin' => true ,
+        ]);
+    } // User::factory()->admin()->create()
 }
